54a9dc2a4896eab654d0da86bc8ca39ceeaa2dd0
Franck Kabasele <kabasele467@gmail.com>
2021-11-04 16:52:28 -0400

    render users from url

--- src/App.js
+++ src/App.js
@@ -6,21 +6,13 @@ class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Frank Einstein',
-          id: '3ewnjsdc'
-        },
-        {
-          name: 'Dracula',
-          id: 'dkncksdc'
-        },
-        {
-          name: 'Katakate',
-          id: 'djnsdfjn'
-        }
-      ],
+      monsters: [],
     }
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(user => this.setState({ monsters: user}))
   }
   render() {
     return (
